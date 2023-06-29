52d1d85da352f130228dc489d593b2e6821e0ac4
chien.bui <chien.bui1@ntq-solution.com.vn>
2023-06-29 09:47:14 +0700

    commit message

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CheckFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Enabled = false;
+            this.txtFolder.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.txtFolder.Location = new System.Drawing.Point(41, 33);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(472, 23);
+            this.txtFolder.TabIndex = 0;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(531, 33);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(140, 23);
+            this.btnSelectFolder.TabIndex = 1;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(41, 74);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(472, 23);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.Text = "setRequired setRequiredAny setCellRequired overridesRequiredGrid setCellRequiredA" +
+    "ny";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(531, 74);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(140, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.ItemHeight = 15;
+            this.lstResults.Location = new System.Drawing.Point(41, 150);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(630, 484);
+            this.lstResults.TabIndex = 4;
+            
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 688);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.txtFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtFolder;
+        private Button btnSelectFolder;
+        private TextBox txtSearch;
+        private Button btnSearch;
+        private ListBox lstResults;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System.Text;
+
+namespace CheckFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lstResults.SelectionMode = SelectionMode.MultiExtended;
+            lstResults.KeyDown += ListBox1_KeyDown;
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string selectedPath = folderBrowserDialog.SelectedPath;
+                txtFolder.Text = selectedPath;
+                Console.WriteLine("Đường dẫn được chọn: " + selectedPath);
+            }
+        }
+
+        private void ListBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                CopySelectedItemsToClipboard(lstResults);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string folderPath = txtFolder.Text; // Thay đổi đường dẫn thư mục cần tìm kiếm
+            string keywords = txtSearch.Text; // Thay đổi các từ khóa cần tìm kiếm, cách nhau bởi dấu cách
+
+            List<string> matchingFiles = SearchFiles(folderPath, keywords);
+
+            if (string.IsNullOrWhiteSpace(folderPath))
+            {
+                MessageBox.Show("Select folder!");
+            }
+
+            if (string.IsNullOrWhiteSpace(keywords))
+            {
+                MessageBox.Show("Input keyword!");
+            }
+
+            string[] searchKeywords = keywords.Split(' ');
+
+            if (matchingFiles.Count > 0)
+            {
+                lstResults.Items.Clear();
+                Console.WriteLine("Matching files:");
+                foreach (string file in matchingFiles)
+                {
+                    Console.WriteLine(file);
+                    Console.WriteLine("Lines with matching keywords:");
+
+                    string[] lines = File.ReadAllLines(file);
+
+                    lstResults.Items.Add(file);
+                    string headerDetail = $"Detail: ";
+                    lstResults.Items.Add(headerDetail);
+
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        foreach (string keyword in searchKeywords)
+                        {
+                            if (lines[i].Contains(keyword))
+                            {
+                                Console.WriteLine("Line {0}: {1}", i + 1, lines[i]);
+                                string lineDetail = $"Line {i + 1}: {lines[i]}";
+                                lstResults.Items.Add(lineDetail);
+
+                                break;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                lstResults.Items.Clear();
+                MessageBox.Show("No matching files found.");
+            }
+
+            Console.ReadLine();
+        }
+
+        static List<string> SearchFiles(string folderPath, string keywords)
+        {
+            List<string> matchingFiles = new List<string>();
+
+            string[] files = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+
+            string[] searchKeywords = keywords.Split(' ');
+
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                bool isMatch = false;
+
+                string[] lines = File.ReadAllLines(file);
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    foreach (string keyword in searchKeywords)
+                    {
+                        if (lines[i].Contains(keyword))
+                        {
+                            isMatch = true;
+                            break;
+                        }
+                    }
+
+                    if (isMatch)
+                    {
+                        matchingFiles.Add(file);
+                        break;
+                    }
+                }
+            }
+
+            return matchingFiles;
+        }
+
+        private void CopySelectedItemsToClipboard(ListBox listBox)
+        {
+            if (listBox.SelectedItems.Count > 0)
+            {
+                // Tạo một chuỗi để lưu trữ dữ liệu được chọn
+                StringBuilder sb = new StringBuilder();
+
+                // Lặp qua các mục được chọn và thêm chúng vào chuỗi
+                foreach (var selectedItem in listBox.SelectedItems)
+                {
+                    sb.AppendLine(selectedItem.ToString());
+                }
+
+                // Sao chép chuỗi vào Clipboard
+                Clipboard.SetText(sb.ToString());
+            }
+        }
+
+        //private void copyButton_Click(object sender, EventArgs e)
+        //{
+        //    CopySelectedItemsToClipboard(lstResults);
+        //}
+
+    }
+}
